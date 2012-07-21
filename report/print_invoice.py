b19b44f9353adfb9eca603a604b71bd2b8377f90
Mariano Ruiz <mrsarm@gmail.com>
2012-07-21 02:26:07 -0300

    Format VAT number conform with Argentine mask (C.U.I.T.)

--- report/print_invoice.py
+++ report/print_invoice.py
@@ -23,12 +23,26 @@
 import time
 from report import report_sxw
 
+
+def format_vat_ar(vat):
+    '''
+    Returns VAT formated conform to Argentine mask (C.U.I.T.).
+    Eg.: "AR20101234569" -> "20-10123456-9" 
+    '''
+    if not vat:
+        return ''
+    if vat[:2] == "AR":
+        return "%s-%s-%s" % (vat[2:4], vat[4:12], vat[12:])
+    return vat
+
 class ar_account_invoice(report_sxw.rml_parse):
     def __init__(self, cr, uid, name, context):
         super(ar_account_invoice, self).__init__(cr, uid, name, context=context)
+        vat = self.pool.get('res.partner').browse(cr, uid, context['partner_id']).vat
         self.localcontext.update({
             'time': time,
             'use_header': False,
+            'vat': format_vat_ar(vat),
         })
 report_sxw.report_sxw(
     'report.ar.account.invoice',
@@ -40,9 +54,11 @@ report_sxw.report_sxw(
 class ar_account_invoice_with_header(report_sxw.rml_parse):
     def __init__(self, cr, uid, name, context):
         super(ar_account_invoice_with_header, self).__init__(cr, uid, name, context=context)
+        vat = self.pool.get('res.partner').browse(cr, uid, context['partner_id']).vat
         self.localcontext.update({
             'time': time,
             'use_header': True,
+            'vat': format_vat_ar(vat),
         })
 report_sxw.report_sxw(
     'report.ar.account.invoice.with.header',
